05fdbab374097707a673575993545cd3d5a31763
mdallen5393 <allen_matthew@outlook.com>
2022-08-30 08:29:46 -0700

    Task 1. Copy - Cut - Paste

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))

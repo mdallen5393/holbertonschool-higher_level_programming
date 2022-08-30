225b81bc457f7379748031b1c8497e93eb10e50d
mdallen5393 <allen_matthew@outlook.com>
2022-08-30 10:24:04 -0700

    Task 4. The last digit

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 last = number % 10
 if number < 0:
-    last = 10 + last * (-1)
+    last = last - 10
 if last == 0:
     line = ('Last digit of ' + str(number) +
             ' is ' + str(last) + ' and is zero')

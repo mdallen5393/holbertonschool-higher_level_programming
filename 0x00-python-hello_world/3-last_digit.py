f84891a12924fda480bf2439cca445fa22787974
mdallen5393 <allen_matthew@outlook.com>
2022-08-30 10:22:34 -0700

    Task 4. The last digit

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -12,7 +12,7 @@ else:
     if last > 5:
         line = ('Last digit of ' + str(number) +
                 ' is ' + str(last) + ' and is greater' +
-                ' than 6 and not 0')
+                ' than 5')
     elif last < 5:
         line = ('Last digit of ' + str(number) +
                 ' is ' + str(last) + ' and is less' +

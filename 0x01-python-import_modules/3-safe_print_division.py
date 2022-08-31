287960afa43e2ef90dedfd3ee11c6d689a712cba
mdallen5393 <allen_matthew@outlook.com>
2022-08-31 11:19:57 -0700

    Task 3. Integers division with debug

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-		c = a / b
-		pass
-	except ZeroDivisionError:
-		c = None
-	finally:
-		print("Inside result: {}".format(c))
-		return c
+    try:
+        c = a / b
+        pass
+    except ZeroDivisionError:
+        c = None
+    finally:
+        print("Inside result: {}".format(c))
+        return c

aaf00e689e118419ad6616e5697998cfe70e8e73
mdallen5393 <allen_matthew@outlook.com>
2022-08-31 09:13:52 -0700

    Initial commit; task 0. Import a simple function from a simple file

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 
-
-a, b = 1, 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import sys
+    a, b = 1, 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

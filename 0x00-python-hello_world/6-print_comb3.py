f633068ef304b8f414238af44a2cf60d4eb17cfd
mdallen5393 <allen_matthew@outlook.com>
2022-08-30 18:23:33 -0700

    Task 8.  8. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i == 9 and j == 9:
-            print("99")
+        if i == 8 and j == 9:
+            print("89")
         if i <= j:
             print("{:d}{:d}".format(i, j), end=", ")

67610d5c03fb59ef61c4cfab3096d1fa36a640d8
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 10:28:01 -0700

    Task 4. Replace in a copy

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
-    if not idx < 0 and not idx >= len(my_list):
-        for num in my_list:
-            new_list.append(num)
-        new_list[idx] = element
-    return new_list
+    if my_list:
+        new_list = []
+        if not idx < 0 and not idx >= len(my_list):
+            for num in my_list:
+                new_list.append(num)
+            new_list[idx] = element
+        return new_list

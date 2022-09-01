f995dabf9ccf6de43ade6c38c628afde6a932bc5
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 10:22:32 -0700

    Task 4. Replace in a copy

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def new_in_list(my_list, idx, element):
+    new_list = None
+    if idx < 0 and idx >= len(my_list):
+        return(my_list)
+    for i in range(len(my_list) - 1):
+        new_list[i] = my_list[i]
+    new_list[idx] = element
+    return new_list

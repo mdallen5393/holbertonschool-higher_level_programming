0196ccc150d5939861fa0f3c964cd0bcde6e40b8
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 10:43:25 -0700

    Task 5. Can you C me now?

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def no_c(my_string):
+    if my_string:
+        new_string = ""
+        for char in my_string:
+            if char != "c" and char != "C":
+                new_string += char
+        return new_string

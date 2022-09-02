9b03f3b53d4a55ac7c663df1c533a558c20c4b4a
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 19:52:18 -0700

    Task 10. Only by 2

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    if my_list:
+        for element in my_list:
+            if element % 2 == 0:
+                element = "True"
+            else:
+                element = "False"
+    return my_list

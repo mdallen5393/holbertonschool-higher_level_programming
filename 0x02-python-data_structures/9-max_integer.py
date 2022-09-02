c5391c0414637e8ca8c970ec23452d5204a5ef1e
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 19:49:23 -0700

    Task 9. Find the max

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    my_list.sort(reverse=True)
-    return my_list[0]
+    if my_list:
+        my_list.sort(reverse=True)
+        return my_list[0]

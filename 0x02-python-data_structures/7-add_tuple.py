e569e855523943485646e155c0d1c3860b16a138
mdallen5393 <allen_matthew@outlook.com>
2022-09-01 19:18:38 -0700

    Task 7. Tuples addition

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
 
+	if len(tuple_a) == 0:
+		a1, a2 = 0, 0
+	elif len(tuple_a) == 1:
+		a1, a2 = tuple_a[0], 0
+	else:
+		a1, a2 = tuple_a[0], tuple_a[1]
+
+	if len(tuple_b) == 0:
+		b1, b2 = 0, 0
+	elif len(tuple_b) == 1:
+		b1, b2 = tuple_b[0], 0
+	else:
+		b1, b2 = tuple_b[0], tuple_a[0]
+
+	return ((a1 + b1, a2 + b2))

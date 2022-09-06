b16cc0acd13e0d939300c1ece50b51200d9f0b8b
mdallen5393 <allen_matthew@outlook.com>
2022-09-06 11:48:48 -0700

    Task 10. Best score

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_key = max(a_dictionary, key=max_key.get)
+    max_key = max(a_dictionary, key=a_dictionary.get)
     if max_key:
         return max_key

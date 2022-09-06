9857b83f5a3299a24457ebad0b636072bc28ee00
mdallen5393 <allen_matthew@outlook.com>
2022-09-06 11:00:23 -0700

    Task 6. Print sorted dictionary

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-	for val in sorted(a_dictionary):
-		print(val, ': ', a_dictionary[val], sep='')
+    for key in sorted(a_dictionary):
+        print(key, ': ', a_dictionary[key], sep='')

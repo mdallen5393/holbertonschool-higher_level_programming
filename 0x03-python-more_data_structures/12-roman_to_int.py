06e8291987048ce126bb25e76f5d4b4a303a8500
mdallen5393 <allen_matthew@outlook.com>
2022-09-06 12:48:15 -0700

    Task 12. Roman to Integer

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-	
+	if roman_string and type(roman_string) == str:
+		length = len(roman_string)
+		rstr = roman_string
+		num = 0
+		nums = {
+			"I":1,
+			"V":5,
+			"X":10,
+			"L":50,
+			"C":100,
+			"D":500,
+			"M":1000
+		}
+		for i in range(length):
+			if i + 1 < length and nums[rstr[i]] < nums[rstr[i + 1]]:
+				num -= nums[rstr[i]]
+			else:
+				num += nums[rstr[i]]
+		return num

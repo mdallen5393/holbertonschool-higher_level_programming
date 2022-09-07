f69a26b452a7b145f62410921223f039831ceea6
mdallen5393 <allen_matthew@outlook.com>
2022-09-07 11:15:15 -0700

    Task 1. Size validation

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -4,16 +4,10 @@
 
 class Square:
     """This class defines a square"""
-    def __init__(self, size=None):
+    def __init__(self, size=0):
         """This initializes a square with a size"""
-        try:
-            int(size)
-            pass
-        except TypeError:
-            print("size must be an integer")
-        try:
-            size < 0
-            pass
-        except ValueError:
-            print("size must be >= 0")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size

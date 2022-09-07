93d3f0e3954ab3bb25dd2dfca3ad5a56a10acdcd
mdallen5393 <allen_matthew@outlook.com>
2022-09-07 11:32:45 -0700

    Task 3. Access and update private attribute

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+"""This is the square class!"""
 
+
+from ctypes import sizeof
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
+
+    def size(self):
+        """Getter for a square's size"""
+        return self.__size
+
+    def size(self, value):
+        """Setter for a square's size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value

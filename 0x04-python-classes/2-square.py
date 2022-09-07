46d7868fb3a86c7355897b5559b4440eda82d939
mdallen5393 <allen_matthew@outlook.com>
2022-09-07 11:24:11 -0700

    Task 2. Area of a square

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""This is the square class!"""
 
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.size ** 2

72ffac81b7c658ebd688297f48406d5c103b998b
mdallen5393 <allen_matthew@outlook.com>
2022-09-08 08:44:06 -0700

    Task 4 test

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -6,12 +6,7 @@ class Square:
     """This class defines a square"""
     def __init__(self, size=0):
         """This initializes a square with a size"""
-        self.size(size)
-        # if type(size) != int:
-        #     raise TypeError("size must be an integer")
-        # if size < 0:
-        #     raise ValueError("size must be >= 0")
-        # self.__size = size
+        self.size = size
 
     def area(self):
         """Prints area"""

c8ab6ba6c83474f86a0a750ba9bbed26dab4d4b3
mdallen5393 <allen_matthew@outlook.com>
2022-09-12 10:25:15 -0700

    Task 3. Print Square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,12 +3,12 @@
 
 
 def print_square(size):
-	""" Function that prints squares using '#' """
-	if type(size) != int:
-		raise TypeError("size must be an integer")
-	if size < 0:
-		raise ValueError("size must be >= 0")
-	for i in range(size):
-		for j in range(size):
-			print("#", end="")
-		print("")
+    """ Function that prints squares using '#' """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")

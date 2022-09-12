3a94ac24d9747ed6c9e0e7c178bc620d71bd9055
mdallen5393 <allen_matthew@outlook.com>
2022-09-12 11:06:02 -0700

    Task 4. Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,10 +3,10 @@
 
 
 def text_indentation(text):
-	""" Function that adds indentation """
-	if type(text) != str:
-		raise TypeError("text must be a string")
-	text = text.replace('. ', ".\n\n")
-	text = text.replace('? ', "?\n\n")
-	text = text.replace(': ', ":\n\n")
-	print(text, end="")
+    """ Function that adds indentation """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    text = text.replace('. ', ".\n\n")
+    text = text.replace('? ', "?\n\n")
+    text = text.replace(': ', ":\n\n")
+    print(text, end="")

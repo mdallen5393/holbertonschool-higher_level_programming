69a5f2274252ad848c9bdf5379106cb56791eabb
mdallen5393 <allen_matthew@outlook.com>
2022-09-12 09:43:36 -0700

    Task 2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,10 @@
 
 
 def say_my_name(first_name, last_name=""):
-	""" Function that prints the input names """
-	
+    """ Function that prints the input names """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
 
-	if type(first_name) != str:
-		raise TypeError("first_name must be a string")
-	if type(last_name) != str:
-		raise TypeError("last_name must be a string")
-
-	print("My name is", first_name, last_name)
+    print("My name is", first_name, last_name)

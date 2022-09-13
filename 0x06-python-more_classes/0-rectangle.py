baf03b0b0473b3f139d7ca51709e68e34fb1a767
mdallen5393 <allen_matthew@outlook.com>
2022-09-13 10:52:43 -0700

    Task 0. Real definition of a rectangle

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" 0-rectangle Module """
+
+
+class Rectangle():
+    """This class defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """This initializes a rectangle with a width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
---
+++ 0x06-python-more_classes/README.md
@@ -0,0 +1,15 @@
+# PROJECT: 0x06. Python - More Classes and Objects
+### AUTHOR: Matthew Allen
+
+## TASKS:
+### 0. Real definition of a rectangle - 0-rectangle.py
+Class `Rectangle` that defines a rectangle.
+
+### 1. Area and Perimeter - 1-rectangle.py
+Class `Rectangle` that defines a rectangle, as well as the `area()` and `perimeter()` methods.
+
+### 2. String representation - 2-rectangle.py
+Class `Rectangle` that defines a rectangle, as well as the `print()` and `str()` methods.
+
+### 3. Eval is magic - 3-rectangle.py
+Class `Rectangle` that defines a rectangle, as well as the `repr()` method.

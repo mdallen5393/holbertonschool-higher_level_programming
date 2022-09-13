13c7679a61142d774f5a377afec209025fbfeae3
mdallen5393 <allen_matthew@outlook.com>
2022-09-13 13:28:30 -0700

    Task 3 test

--- 0x06-python-more_classes/3-rectangle.py
+++ 0x06-python-more_classes/3-rectangle.py
@@ -26,9 +26,13 @@ class Rectangle():
         return 2*self.__width + 2*self.__height
 
     def __str__(self):
-        # if 0 in (self.__width, self.__height):
-        #     return ""
-        return (self.__height * ("#" * self.__width + "\n") + ("#" * self.__width))
+        string = ""
+        if 0 in (self.__width, self.__height):
+            return ""
+        for i in range(self.__height - 1):
+            string += "#" * self.__width + "\n"
+        string += "#" * self.__width
+        return string
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)

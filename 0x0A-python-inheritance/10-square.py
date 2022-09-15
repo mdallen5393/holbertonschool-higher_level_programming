f7994cdc9e59ca34f68cf5a086e4e48924b8b4c4
mdallen5393 <allen_matthew@outlook.com>
2022-09-15 11:22:39 -0700

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" 7-base_geometry Module """
+
+
+class BaseGeometry():
+    """ BaseGeometry class """
+    def area(self):
+        """ raises exception """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class that extends BaseGeometry """
+    def __init__(self, width, height):
+        """ initializer for Rectangle class """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """ Square class that extends Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)

2a01374b1faa69be752560f25935ccdd946958db
mdallen5393 <allen_matthew@outlook.com>
2022-09-15 07:09:57 -0700

    Task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,9 @@
 
 
 def lookup(obj):
+    """
+    Function that returns the list of available
+    attributes and methods of an object
+    """
     atts_and_mets = list(dir(obj))
     return atts_and_mets

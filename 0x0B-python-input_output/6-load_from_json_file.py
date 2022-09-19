4e4cf3cad879f22548ceecc9ec7de585785dea1d
mdallen5393 <allen_matthew@outlook.com>
2022-09-19 10:41:19 -0700

    Task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ def load_from_json_file(filename):
     """
     Function that creates an Object from a "JSON file.
     """
-    with open(filename, mode='r+', encoding='utf-8') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         return json.load(f)

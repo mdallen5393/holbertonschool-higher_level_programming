07f35fdeca07b62115dfc5486b9c5f103c37badc
mdallen5393 <allen_matthew@outlook.com>
2022-09-19 09:16:00 -0700

    Initial commit for project 0x08. Python - Input/Output; task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 
 def read_file(filename=""):
-	with open('my_file_0.txt', encoding="utf-8") as f:
-		print(f.read())
+    with open('my_file_0.txt', encoding="utf-8") as f:
+        print(f.read())

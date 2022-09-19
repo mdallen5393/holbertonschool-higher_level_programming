7d7676ac5d58ae344fdc48afe23740f5e5c2c9c4
mdallen5393 <allen_matthew@outlook.com>
2022-09-19 11:39:55 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""10-student Module"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializer method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of
+        a Student instance
+        """
+        dict = vars(self)
+        if not attrs:
+            return dict
+
+        for item in attrs:
+            if type(item) != str:
+                return dict
+
+        studentInfo = {}
+        for item in attrs:
+            if item in dict:
+                studentInfo[item] = dict[item]
+        return studentInfo

0de2ae0bacce63cd09eb1f39244d61fb104aeaab
mdallen5393 <allen_matthew@outlook.com>
2022-09-20 10:43:08 -0700

    Task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,9 +21,9 @@ class Rectangle(Base):
 
     def display(self):
         """method to display a rectangle using `#`"""
-        print("\n" * (self.__width - 1))
+        print("\n" * (self.__y), end="")
         for i in range(self.__height):
-            print(" " * (self.__x - 1) + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """return string representation of Rectangle"""

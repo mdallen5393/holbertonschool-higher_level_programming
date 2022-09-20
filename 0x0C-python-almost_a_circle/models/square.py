f46ea3d18d46f0ee762fa15a0b8d373992baef0a
mdallen5393 <allen_matthew@outlook.com>
2022-09-20 12:53:58 -0700

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -37,3 +37,18 @@ class Square(Rectangle):
                 self.x = value
             if key == "y":
                 self.y = value
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """Setter for __width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value

fccf5157f0276a9ca94fa9e4ca85d62d285973b7
mdallen5393 <allen_matthew@outlook.com>
2022-09-20 12:40:47 -0700

    Task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,27 @@ class Square(Rectangle):
         return '[' + type(self).__name__ + '] (' + str(self.id) \
             + ') ' + str(self.x) + '/' + str(self.y) + ' - ' \
             + str(self.height)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.height = args[1]
+                self.width = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "size":
+                self.width = value
+                self.height = value
+            if key == "x":
+                self.x = value
+            if key == "y":
+                self.y = value
+

2e1e98cbc6354cfb8f875b98b0126819b4d1079a
mdallen5393 <allen_matthew@outlook.com>
2022-09-20 09:20:04 -0700

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+rectangle module that defines the Rectangle class
+"""
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Getter for __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for __width"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for __height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for __x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for __x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for __y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for __y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value

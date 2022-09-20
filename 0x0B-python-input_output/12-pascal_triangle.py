ba6c6a178851181e5f64ed9bdb7995d4ce715425
mdallen5393 <allen_matthew@outlook.com>
2022-09-19 20:16:08 -0700

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""12-pascal_triangle Module"""
+
+
+def pascal_triangle(n):
+    pascal = []
+
+    for i in range(n):
+        row = []
+        row.append(1)
+        for j in range(2, i + 1):
+            row.append(0)
+        pascal.append(row)
+
+    prev = pascal[0]
+    for i in range(1, n):
+        for j in range(1, i):
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        pascal[i].append(1)
+    return pascal

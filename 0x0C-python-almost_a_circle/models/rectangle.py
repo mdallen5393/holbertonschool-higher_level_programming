7214c876db6e813a38812d03bd52ea0d0f0dda1e
mdallen5393 <allen_matthew@outlook.com>
2022-09-20 09:20:58 -0700

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,9 +4,11 @@ rectangle module that defines the Rectangle class
 """
 from models.base import Base
 
+
 class Rectangle(Base):
     """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializer for Rectangle class"""
         super().__init__(id)
         self.__width = width
         self.__height = height

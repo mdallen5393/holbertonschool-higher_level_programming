653790cfa768909f6d74f25e4273b81e92d6eeb7
mdallen5393 <allen_matthew@outlook.com>
2022-09-21 08:15:36 -0700

    Task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,4 +43,6 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        pass
+        if json_string is None or (len(json_string) == 0):
+            return([])
+        json.loads(json_string)

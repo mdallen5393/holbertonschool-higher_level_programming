ad349c39dcee80a2d125cf7940b9b7ed77e49b14
mdallen5393 <allen_matthew@outlook.com>
2022-09-23 07:54:03 -0700

    updated test methods

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,20 +3,69 @@
 Unittest module for Rectangle class.
 """
 import unittest
-from models.base import Base
+from models.rectangle import Rectangle
 
 class test_rectangle(unittest.TestCase):
     """
     Class test_rectangle that provides unit testing for the `Rectangle`
     class.
     """
-    def test_values(self):
+    @classmethod
+    def setUpClass(cls):
         """
-        Value unit tests.
+        setUp method for testing the Rectangle class.
         """
-        base1 = Base(10)
-        # TODO: add self.assertEqual(max_integer("String"), 't')
+        cls.rect1 = Rectangle(1, 1, 0, 0, 100)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        tearDown method for testing the Rectangle class.
+        """
+        del cls.rect1
+        return super().tearDownClass()
+
+    def test_init(self):
+        """
+        Method for testing initialization of the Rectangle class.
+        """
+        self.assertEqual(self.rect1.id, 100)
+        self.assertEqual(self.rect1.width, 1)
+        self.assertEqual(self.rect1.height, 1)
+        self.assertEqual(self.rect1.x, 0)
+        self.assertEqual(self.rect1.y, 0)
+
+    def test_area(self):
+        """
+        Method for testing the area method of the Rectangle class.
+        """
+        self.assertEqual()
+
+    def test_display(self):
+        """
+        Method for testing the display method of the Rectangle class.
+        """
         pass
+
+    def test_str(self):
+        """
+        Method for testing the __str__ method of the Rectangle class.
+        """
+        pass
+
+    def test_update(self):
+        """
+        Method for testing the update method of the Rectangle class.
+        """
+        pass
+
+    def test_to_dictionary(self):
+        """
+        Method for testing the to_dictionary method of the Rectangle
+        class.
+        """
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()

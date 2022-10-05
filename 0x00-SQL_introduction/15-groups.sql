c592c2789ff948b837ebecf2cf092d9ba1012d1f
mdallen5393 <allen_matthew@outlook.com>
2022-10-05 10:57:30 -0700

    Task 15. Number by score

---
+++ 0x00-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- script that lists the number of records with
+-- the same score in the table `second_table`
+SELECT score,
+    COUNT(*) AS number
+FROM second_table
+GROUP BY score
+HAVING number >= 1

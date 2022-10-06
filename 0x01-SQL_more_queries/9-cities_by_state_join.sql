8ab91857ebb8c232c47c339ff4901fd7b99db204
mdallen5393 <allen_matthew@outlook.com>
2022-10-06 16:19:08 -0700

    Task 9

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database
 -- `htbn_0d_usa`.
 SELECT cities.id, cities.name, states.name
-  FROM cities RIGHT JOIN states
+  FROM cities LEFT JOIN states
     ON cities.state_id = states.id;

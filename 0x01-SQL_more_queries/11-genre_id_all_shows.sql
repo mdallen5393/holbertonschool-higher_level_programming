e095c5cfaea3a87a23cd198cc201649c5f6b7510
mdallen5393 <allen_matthew@outlook.com>
2022-10-06 18:54:52 -0700

    Task 11

---
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in the database
+-- `hbtn_0d_tvshows`.
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres 
+      ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres 
+      ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

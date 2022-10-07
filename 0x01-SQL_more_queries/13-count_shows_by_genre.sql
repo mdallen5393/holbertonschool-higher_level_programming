ff516bf86a721c46aac54af7ca7a2250dc1bf2e3
mdallen5393 <allen_matthew@outlook.com>
2022-10-06 19:28:55 -0700

    Task 13

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT
                 COUNT(*)
              AS number_of_shows
            FROM tv_genres
-      LEFT JOIN tv_show_genres
+     INNER JOIN tv_show_genres
              ON tv_genres.id = tv_show_genres.genre_id
        GROUP BY genre
        ORDER BY number_of_shows DESC;

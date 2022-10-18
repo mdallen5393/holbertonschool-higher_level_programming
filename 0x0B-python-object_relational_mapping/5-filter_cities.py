7216df2dd435ae6e5b68a6d7e802db0d65e1005d
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 12:53:42 -0700

    Task 5. All cities by state

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python3
+"""
+This script lists all `states` from the database
+`hbtn_0e_0_usa` starting with `N`.
+"""
+import MySQLdb
+import sys
+
+
+def cities_by_states():
+    """
+    lists all states
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT name \
+                   FROM cities \
+                  WHERE state_id = (SELECT id \
+                                      FROM states \
+                                    WHERE name=%(target)s);",
+                {'target':sys.argv[4]})
+
+    results = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    for i in range(len(results) - 1):
+        print(results[i][0], end=', ')
+    print(results[len(results) - 1][0])
+
+
+if __name__ == "__main__":
+    cities_by_states()
 

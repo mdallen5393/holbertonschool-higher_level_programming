c2f8b551bf2d1180588400c239f618d305c1374f
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 09:56:04 -0700

    Task 1. Filter states

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -14,6 +14,7 @@ def list_states():
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY id")
     results = cur.fetchall()
 

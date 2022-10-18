aaa3ead1351d9db98d09365f09642c25f4b6791c
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 12:11:26 -0700

    Task 3. SQL Injection...

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+"""
+This script lists all `states` from the database
+`hbtn_0e_0_usa` starting with `N`.
+"""
+import MySQLdb
+import sys
+
+
+def safe_filter_states():
+    """
+    lists all states
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute('SELECT * FROM states WHERE BINARY name = %(state)s \
+ORDER BY id;', {'state':sys.argv[4]})
+
+    results = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    for row in results:
+        print(row)
+
+
+if __name__ == "__main__":
+    safe_filter_states()

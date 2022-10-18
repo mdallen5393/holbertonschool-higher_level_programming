3518190e5085b922deb6a49fdbd9192ef7ea1b1f
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 09:34:42 -0700

    Task 0. Get all states

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ def list_states():
     """
     lists all states
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 

688b3c9228cb1cc312983cdb87e420df0d424b10
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 09:36:43 -0700

    Task 0. Get all states

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,16 @@
 This script lists all `states` from the database
 `hbtn_0e_0_usa`.
 """
-
 import MySQLdb
 import sys
+
 
 def list_states():
     """
     lists all states
     """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id")

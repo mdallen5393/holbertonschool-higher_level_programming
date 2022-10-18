e1b40e1377c5c15c6804edfd3876a0f031aa6afd
mdallen5393 <allen_matthew@outlook.com>
2022-10-18 10:12:55 -0700

    Task 2. Filter states by user input

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
+"""
+This script lists all `states` from the database
+`hbtn_0e_0_usa` starting with `N`.
+"""
+import MySQLdb
+import sys
 
+
+def list_states():
+    """
+    lists all states
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute(f"SELECT * FROM states WHERE name = '{sys.argv[4]}' \
+ORDER BY id;")
+    results = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    for row in results:
+        print(row)
+
+
+if __name__ == "__main__":
+    list_states()

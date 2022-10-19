0b328ae3e2e32ec4e9f5bc6ff664b8fce3d60b17
mdallen5393 <allen_matthew@outlook.com>
2022-10-19 09:55:14 -0700

    Task 10. Get a state

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+"""
+Script to list all states in the states table.
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
 
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    qry = session.query(State
+                        ).order_by(State.id).filter(State.name == sys.argv[4])
+    if qry is not None:
+        for state in qry:
+            print(f"{state.id}: {state.name}")
+    else:
+        print("Not found")

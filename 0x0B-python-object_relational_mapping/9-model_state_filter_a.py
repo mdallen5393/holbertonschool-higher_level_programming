3d02a1f4fb127fde16f9b39e964a1b67e4c87341
mdallen5393 <allen_matthew@outlook.com>
2022-10-19 09:32:39 -0700

    Task 9. Contains `a`

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
+"""
+Script to list all states in the states table.
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
 
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print(f"{state.id}: {state.name}")
+
+    for state in session.query(State).order_by(State.id):
+        if 'a' in state.name:
+            print(f"{state.id}: {state.name}")

4056f0298f1f9a1b2d70259a255d58fcf4898f55
mdallen5393 <allen_matthew@outlook.com>
2022-10-20 06:14:58 -0700

    Task 11. Add a new state

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
 
     session.add(State(name="Louisiana"))
     state = session.query(State)\
-        .filter_by(name='Louisiana')\
+        .filter(State.name == 'Louisiana')\
         .order_by(State.id)[-1]
     print(state.id)

bc0523d1f22c239cada2638c2c5719149daf70ea
mdallen5393 <allen_matthew@outlook.com>
2022-10-20 06:22:21 -0700

    Task 12. Update a state

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
+"""
+Script to list all states in the states table.
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+import sys
 
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name="Louisiana"))
+    state = session.query(State).filter(State.id == 2)[0]
+    state.name = "New Mexico"
+
+    session.commit()

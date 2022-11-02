9a345ab8d7b49ba7215f0c1188d36b4d57ccb6e7
mdallen5393 <allen_matthew@outlook.com>
2022-11-02 07:25:42 -0700

    initial commit for project 0x0C. Python - Network #1; task 0

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     req = request.Request('https://intranet.hbtn.io/status')
     with request.urlopen(req) as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))

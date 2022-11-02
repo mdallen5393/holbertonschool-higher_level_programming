9d753e0c49b734c20217b81f6dfc150564b104f4
mdallen5393 <allen_matthew@outlook.com>
2022-11-02 06:38:52 -0700

    initial commit for project 0x0C. Python - Network #1; task 0

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -2,8 +2,10 @@
 """
 Script that fetches `https://intranet.hbtn.io/status`.
 """
+from urllib import request
+
+
 if __name__ == "__main__":
-    from urllib import request
     req = request.Request('https://intranet.hbtn.io/status')
     with request.urlopen(req) as response:
         html = response.read()

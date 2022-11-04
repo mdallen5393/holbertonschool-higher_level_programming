3fdccf82ad62f173e4b0e7b63a45e2fd55b43821
mdallen5393 <allen_matthew@outlook.com>
2022-11-03 21:25:06 -0700

    Task 8

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a
+`POST` request to `http://0.0.0.0:5000/search_user`
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        q = ""
+    if len(sys.argv) != 1:
+        q = sys.argv[1]
+
+    http = requests.post(url, data={'q':q})
+    try:
+        my_dict = http.json()
+        id = my_dict.get('id')
+        name = my_dict.get('name')
+        if len(dict) == 0:
+            print("No result")
+        if len(dict) != 0:
+            print("[{}] {}".format(id, name))
+    except Exception as e:
+        print("Not a valid JSON")

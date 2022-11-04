80f2b6208050bed834d28f8044ef4e7bdcaa61f9
mdallen5393 <allen_matthew@outlook.com>
2022-11-03 21:10:41 -0700

    task 3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         with request.urlopen(http) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(f"Error code: {e.code}")
+        print("Error code: {}".format(e.code))

8ead3610ff6b31f678500f76a8811e0b10ebdbb1
mdallen5393 <allen_matthew@outlook.com>
2022-11-03 21:28:09 -0700

    Task 10

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes GitHub credentials (username
+and password) and uses the GitHUB API to display its `id`.
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = sys.argv[1]
+    token = sys.argv[2]
+    http = requests.get(url, auth=HTTPBasicAuth(username, token)).json()
+    print(requests.get('id'))

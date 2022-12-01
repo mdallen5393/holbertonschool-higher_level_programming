25b64ae9a89375bcdea877c864de5bd08b7d219f
mdallen5393 <allen_matthew@outlook.com>
2022-12-01 15:59:37 -0800

    Task 12. Object

--- 0x00-javascript-warm_up/12-object.js
+++ 0x00-javascript-warm_up/12-object.js
@@ -3,6 +3,6 @@ const myObject = {
     type: 'object',
     value: 12
   };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);

535c6b31801f9fe02bee45807e729c13b0aea52b
mdallen5393 <allen_matthew@outlook.com>
2022-12-05 09:18:44 -0600

    Task 6

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,7 @@
 const Sq = require('./5-square.js');
 
 class Square extends Sq {
-  charPrint (c) {
-    if (!c) {
-      c = 'X';
-    }
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }

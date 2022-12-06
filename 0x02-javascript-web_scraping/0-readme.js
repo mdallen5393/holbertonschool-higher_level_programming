8ea95459a102ffd5cc4e757ed2e28c299a697109
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 10:16:27 -0600

    Task 0

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -2,9 +2,6 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
-    console.log(err);
-  }
+  if (err) throw err;
+  console.log(data);
 });

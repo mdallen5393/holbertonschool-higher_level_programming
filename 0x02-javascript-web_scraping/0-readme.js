db518b53c32d5af82bb65ff15f440a5fd4dd0f38
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 10:10:08 -0600

    Initial commit for Project 0x02. JavaScript - Web scraping; Task 0. Readme

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let fs = require('fs');
+const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (data) {
     console.log(data);
   } else {

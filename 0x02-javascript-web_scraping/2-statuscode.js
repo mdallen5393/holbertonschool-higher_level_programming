afa960ee34b353492e1525c4014385a961db64d9
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 11:03:26 -0600

    Task 2

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request (url, function (err, response, body) {
+request(url, function (err, response, body) {
   if (err) throw err;
   console.log(response.statusCode);
 });

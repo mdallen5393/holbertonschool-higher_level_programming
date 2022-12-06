bdd850be6a6fa2873fb632c2e8915cf2764a98ce
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 12:21:34 -0600

    Task 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2]
+const url = process.argv[2];
 
 request(url, function (err, response, body) {
   if (err) throw err;
   const data = JSON.parse(body).results;
   let count = 0;
-  for (movie in data) {
-    const chars = data[movie].characters
-      // console.log(JSON.parse(body).title)
-    for (char in chars) {
+  for (const movie in data) {
+    const chars = data[movie].characters;
+    // console.log(JSON.parse(body).title)
+    for (const char in chars) {
       if (chars[char].includes('/18/')) {
         count++;
       }
@@ -18,4 +18,3 @@ request(url, function (err, response, body) {
   }
   console.log(count);
 });
-

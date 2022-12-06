40d76c10ca9b0a093df7f13f8201a8897bc780b0
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 12:59:57 -0600

    Task 5

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+
+
+request(url, function (err, response, body) {
+  if (err) throw err;
+  fs.writeFile(process.argv[3], body, function (err) {
+    if (err) throw err;
+  });
+});

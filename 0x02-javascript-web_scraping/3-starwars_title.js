0a54774d946fde0985cd88d92b32ac23f77ecef1
mdallen5393 <allen_matthew@outlook.com>
2022-12-06 11:34:46 -0600

    Task 3

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-id = process.argv[2]
+const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
 request(url, function (err, response, body) {

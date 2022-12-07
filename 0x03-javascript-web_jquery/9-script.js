bf1ae18cfe94b9f68b009cbe61727f20f610f01b
mdallen5393 <allen_matthew@outlook.com>
2022-12-07 14:16:12 -0600

    Task 9

--- 0x03-javascript-web_jquery/9-script.js
+++ 0x03-javascript-web_jquery/9-script.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-
+const $ = window.$;
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://stefanbohacek.com/hellosalut/?lang=fr',
+    success: ((data) => $('DIV#hello').text(data.hello))
+  });
+});

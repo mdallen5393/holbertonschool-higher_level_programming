60ac4a1a910df5ca5dc239102cf21bb2cb115e48
mdallen5393 <allen_matthew@outlook.com>
2022-12-07 12:23:08 -0600

    Task 7

--- 0x03-javascript-web_jquery/7-script.js
+++ 0x03-javascript-web_jquery/7-script.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-
+const $ = window.$;
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data, status) {
+    $('DIV#character').text(data.name);
+  });
+});

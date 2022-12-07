f0be9e006d290d6171b32f952d05923573316069
mdallen5393 <allen_matthew@outlook.com>
2022-12-07 13:13:33 -0600

    Task 8

--- 0x03-javascript-web_jquery/8-script.js
+++ 0x03-javascript-web_jquery/8-script.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-
+const $ = window.$;
+$(function () {
+  const $movies = $('UL#list_movies');
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+    success: function (movies) {
+      $.each(movies.results, function (i, movie) {
+        $movies.append('<li>' + movie.title + '</li>');
+      });
+    }
+  });
+});

ff204ab724a5f5132b5dddec84f0e103f369932c
mdallen5393 <allen_matthew@outlook.com>
2022-12-07 10:59:22 -0600

    Task 4

--- 0x03-javascript-web_jquery/4-script.js
+++ 0x03-javascript-web_jquery/4-script.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+const $ = window.$;
+$('DIV#toggle_header').click(function () {
+  if ($(this).hasClass('red')) {
+    $(this).removeClass('red');
+    $(this).addClass('green');
+  } else {
+    $(this).removeClass('green');
+    $(this).addClass('red');
+  }
+});
